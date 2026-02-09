3814eb03ffa1914a9b943428878342f4b9a0dffb
Daniel Charles <danielcharles_500@hotmail.com>
2026-02-09 10:11:17 +0000

    Add SQLAlchemy engine and session factory

---
+++ app/db/session.py
@@ -0,0 +1,6 @@
+from app.core.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
