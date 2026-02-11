53f763ca667e3f15984218ba8f7a4e7eb9883d9e
Daniel Charles <danielcharles_500@hotmail.com>
2026-02-11 15:06:37 +0000

    Add name field to Workout model

--- app/models/workout.py
+++ app/models/workout.py
@@ -1,7 +1,10 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 from app.db.base import Base
 
 class Workout(Base):
-    __tablename__ = "workout"
+    __tablename__ = "workouts"
 
     id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+
