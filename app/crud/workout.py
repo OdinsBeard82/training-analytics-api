441168f5d8c4d169cc786290d9c5f13fbbeb2d31
Daniel Charles <danielcharles_500@hotmail.com>
2026-02-13 09:29:56 +0000

    Add transactional create_workout with IntegrityError handling

---
+++ app/crud/workout.py
@@ -0,0 +1,23 @@
+from app.models.workout import Workout
+from app.schemas.workout import WorkoutCreate
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
+
+
+def create_workout(db: Session, workout: WorkoutCreate):
+    
+    db_workout = Workout(**workout.model_dump())
+    db.add(db_workout)
+
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="commit failed")
+
+    db.refresh(db_workout)
+    return db_workout
+    
+    
+    
