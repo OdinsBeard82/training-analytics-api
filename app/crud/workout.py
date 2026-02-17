1cdbe2410ee63cf6d26931620acd9a5b0055288e
Daniel Charles <danielcharles_500@hotmail.com>
2026-02-17 10:20:50 +0000

    Add get_workout_by_id function to Workout CRUD

--- app/crud/workout.py
+++ app/crud/workout.py
@@ -6,7 +6,6 @@ from sqlalchemy.exc import IntegrityError
 
 
 def create_workout(db: Session, workout: WorkoutCreate):
-    
     db_workout = Workout(**workout.model_dump())
     db.add(db_workout)
 
@@ -18,6 +17,8 @@ def create_workout(db: Session, workout: WorkoutCreate):
 
     db.refresh(db_workout)
     return db_workout
-    
-    
-    
+
+
+def get_workout_by_id(db: Session, workout_id: int):
+    return db.get(Workout, workout_id)
+
